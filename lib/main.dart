aff903ffd4449e6eb3be2c6a14cb323492ab04ae
winnicius-moura <winnicius.moura@gmail.com>
2023-02-13 11:34:03 -0300

    TextField titulo e valor

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,30 @@ class MyHomePage extends StatelessWidget {
                   ]),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Título',
+                    ),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor R\$',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(onPressed: () {}, child: const Text('Nova Transação')),
+                    ],
+                  )
+                ]),
+              ),
             )
           ],
         ));
